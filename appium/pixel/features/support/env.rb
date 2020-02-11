42c3c705feec3d1dbc94d57eabef4a43c2d48f92
Priscila Caimi <paraujocaimi@hotmail.com>
2020-02-11 17:00:09 -0300

    Feature funcionando

--- appium/pixel/features/support/env.rb
+++ appium/pixel/features/support/env.rb
@@ -2,6 +2,6 @@ require "appium_lib"
 require "appium_console"
 require "pry"
 
-caps = Appium.load_appium_txt file: File.expand_path("..\\..\\config\\caps\\appium.txt", __dir__), verbose: true
+caps = Appium.load_appium_txt file: File.expand_path("caps/appium.txt", __dir__), verbose: true
 Appium::Driver.new(caps,true)
 Appium.promote_appium_methods Object
