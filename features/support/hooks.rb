f3a7319c6c609e7f134063f8666089c00acad0f1
Priscila Caimi <paraujocaimi@hotmail.com>
2020-02-14 12:01:47 -0300

    atualizando o report para o modelo HTML

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,5 @@
-
+require 'report_builder'
+require 'date'
 
 Before do
   driver.start_driver
@@ -7,6 +8,7 @@ Before do
   device_type = "android"
   @screen = DroidScreens.new if device_type.eql?("android")
   @screen = IOSScreen.new if device_type.eql?("ios")
+  @path_screenshots = "report/screenshot"
 end
 
 Before("@login") do
@@ -22,8 +24,39 @@ Before("@clean_cart") do
   PixelApi.new.remove_my_cart(@user[:email])
 end
 
+# After execute all scenario will take a screenshot
 After do
   screenshot = driver.screenshot_as(:base64)
   embed(screenshot, "image/png", "Screenshot")
   driver.quit_driver
 end
+
+#After with step will take a screenshot of page
+AfterStep do
+  screenshot = driver.screenshot_as(:base64)
+  embed(screenshot, "image/png", "Screenshot")
+end
+
+# https://github.com/rajatthareja/ReportBuilder
+at_exit do
+
+  @infos = {
+    device: "Android",
+    environment: "Dev",
+    Data: Time.now.to_s,
+    include_images:  true,
+    voice_commands: true
+  }
+
+  ReportBuilder.configure do |config |
+    config.input_path = 'report/json/cucumber.json'
+    config.report_path = 'report/html/test_'
+    config.report_types = [:html,:json,:pdf]
+    config.report_title = 'Pixel App'
+    config.additional_info = @infos
+    config.color = "red"
+  end
+  ReportBuilder.build_report
+end
+
+
